f9cf0cc7b50336e5dd1b55dd995146162f3315bc
Akulav <43496076+Akulav@users.noreply.github.com>
2020-06-19 11:34:37 +0300

    Initial (maybe final) release

---
+++ dsl.py
@@ -0,0 +1,118 @@
+import pickle
+import json
+import random
+import tensorflow
+import tflearn
+import numpy
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+
+
+Niswanob1=fuscat1()
+with open('LOCATION') as file: #pune locatie custom numaidecit. wont work with current folder on windows
+ Chri1=Niswanob2.Chri2(file)
+
+try:
+ with open('data.pickle','rb') as fuscat2:
+  (bitboostdotcom1,Chri4,fuscat3,bitboostdotcom2)=ordemo1.Chri2(fuscat2)
+
+except:
+ bitboostdotcom1=[]
+ Chri4=[]
+ bitboostdotcom3=[]
+ Chri3=[]
+ for fuscat5 in Chri1['intents']:
+  for ordemo3 in fuscat5['patterns']:
+   bitboostdotcom4=bitboostdotcom7.bitboostdotcom5(ordemo3)
+   bitboostdotcom1.extend(bitboostdotcom4)
+   bitboostdotcom3.append(bitboostdotcom4)
+   Chri3.append(fuscat5['tag'])
+
+  if (fuscat5['tag']	not in	Chri4):
+   Chri4.append(fuscat5['tag'])
+
+
+ bitboostdotcom1=[Niswanob1.ordemo2(Niswanob4.lower()) for Niswanob4 in       bitboostdotcom1 if (Niswanob4!='?') ]
+ bitboostdotcom1=bitboostdotcom6(list(Niswanob3(bitboostdotcom1)))
+ Chri4=bitboostdotcom6(Chri4)
+ fuscat3=[]
+ bitboostdotcom2=[]
+ Chri5=[0 for 
+Chri6 in       range(len(Chri4))]
+ for (bitboostdotcom8,fuscat4) in enumerate(bitboostdotcom3):
+  bitboostdotcom9=[]
+  bitboostdotcom4=[Niswanob1.ordemo2(Niswanob4.lower()) for Niswanob4 in       fuscat4]
+  for Niswanob4 in bitboostdotcom1:
+   if (Niswanob4	in	bitboostdotcom4):
+    bitboostdotcom9.append(1)
+   else:
+    bitboostdotcom9.append(0)
+
+
+  fuscat6=Chri5[:]
+  fuscat6[Chri4.index(Chri3[bitboostdotcom8])]=1
+  fuscat3.append(bitboostdotcom9)
+  bitboostdotcom2.append(fuscat6)
+
+ fuscat3=Niswanob7.Niswanob5(fuscat3)
+ bitboostdotcom2=Niswanob7.Niswanob5(bitboostdotcom2)
+ with open('data.pickle','wb') as fuscat2:
+  ordemo1.fuscat8((bitboostdotcom1,Chri4,fuscat3,bitboostdotcom2),fuscat2)
+
+
+bitboostdotcomA.fuscat7()
+Niswanob6=Niswanob8.bitboostdotcomB(shape=[None,len(fuscat3[0])])
+Niswanob6=Niswanob8.bitboostdotcomC(Niswanob6,80)
+Niswanob6=Niswanob8.bitboostdotcomC(Niswanob6,80)
+Niswanob6=Niswanob8.bitboostdotcomC(Niswanob6,len(bitboostdotcom2[0]),activation='softmax')
+Niswanob6=Niswanob8.ordemo4(Niswanob6)
+ordemo6=Niswanob8.bitboostdotcomD(Niswanob6)
+ordemo6.bitboostdotcomF(fuscat3,bitboostdotcom2,n_epoch=100,batch_size=80,show_metric=True)
+ordemo6.Chri7('model.tflearn')
+def Niswanob9(NiswanobA,Chri8):
+ fuscat9=[0 for    bitboostdotcomE in       range(len(Chri8))]
+ fuscatB=bitboostdotcom7.bitboostdotcom5(NiswanobA)
+ fuscatB=[Niswanob1.ordemo2(ordemo5.lower()) for ordemo5 in       fuscatB]
+ for ordemo7 in fuscatB:
+  for (Chri9,fuscatA) in enumerate(Chri8):
+   if (fuscatA==ordemo7):
+    fuscat9[Chri9]=1
+
+
+
+ return Niswanob7.Niswanob5(fuscat9)
+
+def ChriB(update,ordemo8):
+ bitboostdotcom10=update.fuscatC.text
+ print bitboostdotcom10
+ update.fuscatC.NiswanobB(ordemo9(bitboostdotcom10))
+
+def ordemo9(ChriA):
+ ChriC=ordemo6.NiswanobD([Niswanob9(ChriA,bitboostdotcom1)])[0]
+ fuscatD=Niswanob7.fuscatE(ChriC)
+ bitboostdotcom12=Chri4[fuscatD]
+ if (ChriC[fuscatD]>0.4):
+  for bitboostdotcom11 in Chri1['intents']:
+   if (bitboostdotcom11['tag']==bitboostdotcom12):
+    NiswanobC=bitboostdotcom11['responses']
+
+
+  print NiswanobE.ChriD(NiswanobC)
+  ChriF=NiswanobE.ChriD(NiswanobC)
+  print ChriC[fuscatD]
+ else:
+  print "The answer's accuracy is less than 40%. Please ask smth else."
+  ChriF="The answer's accuracy is less than 40%. Please ask smth else."
+
+ return ChriF
+
+ordemoA=NiswanobF('1174222095:AAFrJJiOqvJgYt853S8324yUpX8VqjHcJKQ',use_context=True)
+ordemoA.bitboostdotcom13.ChriE(Niswanob10(Chri10.text,ChriB))
+ordemoA.Niswanob12()
+ordemoA.fuscat10()
+
+
+
